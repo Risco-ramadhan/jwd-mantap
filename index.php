e27e2d2244014c177a867cc6601f8e1993597f2a
muhammadsyafiqakmal06 <akmal.indo456@gmail.com>
2023-08-17 22:24:10 +0700

    Table Baru

--- index.php
+++ index.php
@@ -11,46 +11,38 @@
 
 <body class="bg-light">
 
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card shadow">
-                    <div class="card-header bg-primary text-white text-center">
-                        <h4>Login</h4>
-                    </div>
-                    <div class="card-body">
-                        <!-- <div class="text-center mb-3">
-                            <img src="URL_LOGO_UAD" alt="Logo UAD" class="img-fluid rounded-circle" style="max-width: 150px;">
-                        </div> -->
-                        <form>
-                            <div class="mb-3">
-                                <label for="username" class="form-label">Username</label>
-                                <input type="text" class="form-control" id="username" name="username" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" name="password" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-block">Login</button>
-                            <hr>
-                           </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="container p-3">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nim</th>
+                    <th scope="col">Nama Mahasiswa</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry the Bird</td>
+                    <td>Larry the Bird</td>
+                </tr>
+            </tbody>
+        </table>
+
     </div>
 
     <!-- Link ke JavaScript Bootstrap 5 (Opsional jika ingin menggunakan komponen yang memerlukan JavaScript) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
-        function loginWithGoogle() {
-            // Implementasi login dengan akun Google
-        }
-
-        function loginWithFacebook() {
-            // Implementasi login dengan akun Facebook
-        }
-    </script>
 </body>
 
 </html>
